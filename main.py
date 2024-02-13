b153a79cb18f7b22cb0b5d2ab6bf2a6a664afa1f
Ilya Epshteyn <i.a.epshtein@gmail.com>
2024-02-12 23:56:56 -0500

    send a message when a job is done

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ from processing.message_processing import process_raw_single_search_request
 
 from dao_communication.requests_checkout import check_done_requests, update_raw_request
 
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
 def background_check():
     while(True):
         done_requests = check_done_requests()
         for raw_request in done_requests:
+            bot.send_message(raw_request["chat_id"], f'DONE: {raw_request["request"]}')
             update_raw_request(raw_request["id"], 'SENT', raw_request["search_id"])
         time.sleep(1)
-
-bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
