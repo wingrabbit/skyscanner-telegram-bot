22b3a0fb36a5fcdb7c611e719c0f382f94980457
Ilya Epshteyn <i.a.epshtein@gmail.com>
2024-02-12 21:08:57 -0500

    prepare backgound server interaction in a separate thread

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import telebot
+from threading import Thread
+import time
 
 from config import BOT_TOKEN
 from message_parser import parse_raw_single_search_request
 from processing.message_processing import process_raw_single_search_request
+
+def background_check():
+    while(True):
+        time.sleep(1)
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
@@ -10,4 +16,5 @@ bot = telebot.TeleBot(BOT_TOKEN)
 def echo_all(message):
     bot.reply_to(message, process_raw_single_search_request(message))
 
+Thread(target=lambda: background_check()).start()
 bot.infinity_polling()
